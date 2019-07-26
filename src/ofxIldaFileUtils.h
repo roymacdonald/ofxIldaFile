5b325c9db446ec200ddc18a266b9398279b16697
Roy Macdonald <macdonald.roy@gmail.com>
2019-07-26 01:56:45 -0400

    added to utils decrement and wrap function

--- src/ofxIldaFileUtils.h
+++ src/ofxIldaFileUtils.h
@@ -15,3 +15,11 @@ inline void setShortIntoChars(vector<char>& b, short s, size_t index){
 	b[index] = ((s >> 8) & 0xFF);
 	b[index+1] = (s & 0xFF);
 }
+inline void decrementAndWrap(size_t& i, const size_t& limit){
+	if(i == 0){
+		i = limit - 1;
+	}else{
+		i -- ;
+	}
+}
+
