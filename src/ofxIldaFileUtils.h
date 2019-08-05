46574cb131685411f35c6926aff0170ef4621625
Roy Macdonald <macdonald.roy@gmail.com>
2019-08-05 09:02:43 -0400

    added to utils: get bounding box for various types, and increase and decrease index functions

--- src/ofxIldaFileUtils.h
+++ src/ofxIldaFileUtils.h
@@ -8,6 +8,7 @@
 
 #pragma once
 #include "ofxIldaFileConstants.h"
+#include "ofxIldaPoint.h"
 inline short getShortFromChars(char* b, size_t index){
 	return ((b[index] & 0xFF) << 8) | (b[index+1] & 0xFF);
 }
@@ -16,11 +17,16 @@ inline void setShortIntoChars(vector<char>& b, short s, size_t index){
 	b[index] = ((s >> 8) & 0xFF);
 	b[index+1] = (s & 0xFF);
 }
-inline void decrementAndWrap(size_t& i, const size_t& limit){
-	if(i == 0){
-		i = limit - 1;
+inline void incrementAndWrap(size_t& index,  const size_t& limit, int step = 1){
+	index = (index+step)%limit;
+}
+inline void decrementAndWrap(size_t& index, const size_t& limit, int step = 1){
+	if(step < limit){
+	if(index == 0){
+		index= limit - step;
 	}else{
-		i -- ;
+		index -= step ;
+	}
 	}
 }
 
@@ -36,4 +42,98 @@ inline bool ifFormatTrueColor(const ofxIldaFileFormat& format){
 }
 inline bool ifFormatIndexedColor(const ofxIldaFileFormat& format){
 	return (format == OFX_ILDAFILE_FORMAT_3D_INDEXED_COLOR || format == OFX_ILDAFILE_FORMAT_2D_INDEXED_COLOR);
+}
+
+static ofRectangle getBoundingBox(const std::vector<ofPath>& paths){
+	ofRectangle bb;
+	bool bFirst = true;
+	for(auto& p: paths){
+		auto& ol = p.getOutline();
+		for(auto& o: ol){
+			auto& vt = o.getVertices();
+			for(auto& v: vt){
+				if(bFirst){
+					bb.set(v, 0,0);
+					bFirst = false;
+				}else{
+					bb.growToInclude(v);
+				}
+			}
+		}
+	}
+	return bb;
+}
+static ofRectangle getBoundingBox(const ofPath & path){
+	ofRectangle bb;
+	bool bFirst = true;
+	auto& ol = path.getOutline();
+	for(auto& o: ol){
+		auto& vt = o.getVertices();
+		for(auto& v: vt){
+			if(bFirst){
+				bb.set(v, 0,0);
+				bFirst = false;
+			}else{
+				bb.growToInclude(v);
+			}
+		}
+	}
+	
+	return bb;
+}
+static ofRectangle getBoundingBox(const std::vector<ofPolyline> & polys){
+	ofRectangle bb;
+	bool bFirst = true;
+	//	auto& ol = path.getOutline();
+	for(auto& o: polys){
+		auto& vt = o.getVertices();
+		for(auto& v: vt){
+			if(bFirst){
+				bb.set(v, 0,0);
+				bFirst = false;
+			}else{
+				bb.growToInclude(v);
+			}
+		}
+	}
+	
+	return bb;
+}
+static ofRectangle getBoundingBox(const ofPolyline & poly){
+	ofRectangle bb;
+	bool bFirst = true;
+	auto& vt = poly.getVertices();
+	for(auto& v: vt){
+		if(bFirst){
+			bb.set(v, 0,0);
+			bFirst = false;
+		}else{
+			bb.growToInclude(v);
+		}
+	}
+	return bb;
+}
+static ofRectangle getBoundingBox(const ofMesh & mesh){
+	ofRectangle bb;
+	bool bFirst = true;
+	auto& vt = mesh.getVertices();
+	for(auto& v: vt){
+		if(bFirst){
+			bb.set(v, 0,0);
+			bFirst = false;
+		}else{
+			bb.growToInclude(v);
+		}
+	}
+	return bb;
+}
+static ofRectangle getBoundingBox(const std::vector<ofxIlda::Point> & points){
+	ofRectangle bb;
+	if(points.size()){
+		bb.set(points[0].x,points[0].y,0,0);
+		for(auto & p : points){
+			bb.growToInclude(p.x, p.y);
+		}
+	}
+	return bb;
 }
